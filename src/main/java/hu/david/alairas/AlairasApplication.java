61136d77ebe9eca3a87461d75a60d78f8618d792
Szegedi Dávid <szegedi.david@innobyte.hu>
2019-11-19 18:01:02 +0100

    DB impl.

--- src/main/java/hu/david/alairas/AlairasApplication.java
+++ src/main/java/hu/david/alairas/AlairasApplication.java
@@ -11,3 +11,27 @@ public class AlairasApplication {
 	}
 
 }
+
+
+
+
+/* db info
+
+
+Host
+ec2-46-137-120-243.eu-west-1.compute.amazonaws.com
+Database
+d2vus8chuulrbn
+User
+wjhshpewgcgbdg
+Port
+5432
+Password
+5a44df86cc0d449ccdcdda6b994ef50433a4a945eacd4cacda02ef5d9693d6c2
+URI
+postgres://wjhshpewgcgbdg:5a44df86cc0d449ccdcdda6b994ef50433a4a945eacd4cacda02ef5d9693d6c2@ec2-46-137-120-243.eu-west-1.compute.amazonaws.com:5432/d2vus8chuulrbn
+Heroku CLI
+heroku pg:psql postgresql-shaped-77444 --app alairas
+
+
+ */
---
+++ src/main/java/hu/david/alairas/entity/Alairas.java
@@ -0,0 +1,71 @@
+package hu.david.alairas.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "ALAIRAS")
+public class Alairas {
+
+  @Id
+  @GeneratedValue
+  private Integer id;
+
+  @Size(max = 32)
+  private String vezeteknev;
+
+  @Size(max = 16)
+  @Column(nullable = false)
+  private String keresztnev_1;
+
+  @Size(max = 16)
+  @Column(nullable = false)
+  private String keresztnev_2;
+
+  @Column(nullable = false)
+  private Boolean nem;
+
+  public Integer getId() {
+    return id;
+  }
+
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
+  public String getVezeteknev() {
+    return vezeteknev;
+  }
+
+  public void setVezeteknev(String vezeteknev) {
+    this.vezeteknev = vezeteknev;
+  }
+
+  public String getKeresztnev_1() {
+    return keresztnev_1;
+  }
+
+  public void setKeresztnev_1(String keresztnev_1) {
+    this.keresztnev_1 = keresztnev_1;
+  }
+
+  public String getKeresztnev_2() {
+    return keresztnev_2;
+  }
+
+  public void setKeresztnev_2(String keresztnev_2) {
+    this.keresztnev_2 = keresztnev_2;
+  }
+
+  public Boolean getNem() {
+    return nem;
+  }
+
+  public void setNem(Boolean nem) {
+    this.nem = nem;
+  }
+}

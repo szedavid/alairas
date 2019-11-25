73e7b242759c63502e9cec83c72825a828d29dee
Szegedi Dávid <szegedi.david@innobyte.hu>
2019-11-25 12:48:31 +0100

    fix teszt

--- src/main/java/hu/david/alairas/controller/AlairasController.java
+++ src/main/java/hu/david/alairas/controller/AlairasController.java
@@ -5,6 +5,7 @@ import hu.david.alairas.service.AlairasService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,54 +26,54 @@ public class AlairasController {
 //    this.utonevService = utonevService;
   }
 
-//  @GetMapping("/")
-//  public String getAlairasok(Model model) {
-//    model.addAttribute("alairasok", alairasService.findAll());
-//    return "alairasok"; // ez a html template jelenjen meg
-//  }
+  @GetMapping("/")
+  public String getAlairasok(Model model) {
+    model.addAttribute("alairasok", alairasService.findAll());
+    return "alairasok"; // ez a html template jelenjen meg
+  }
 
-//  @GetMapping("/{id}")
-//  public String getAlairas(@PathVariable Integer id, Model model) {
+  @GetMapping("/{id}")
+  public String getAlairas(@PathVariable Integer id, Model model) {
+    Alairas alairas = alairasService.findOne(id)
+        .orElseThrow(() -> new ResourceNotFoundException("Aláírás azonosító nem található: " + id));
+
+    model.addAttribute("alairas", alairas);
+    return "alairas"; // ez a html template jelenjen meg
+  }
+
+  @GetMapping("/hozzaadas")
+  public String hozzaadas(Model model) {
+//    model.addAttribute("utonevek", utonevService.findAll());
+    return "hozzaadas";
+  }
+
+  @PostMapping("/hozzaadas")
+  public String hozzaadas(@RequestParam String vezeteknev, @RequestParam Integer utonevId,
+      @RequestParam(required = false) Integer utonev_2Id,
+      @RequestParam(required = false) Boolean nemeNo, Model model) {
+    Alairas alairas = alairasService.addOne(vezeteknev, utonevId, utonev_2Id, nemeNo);
+    return getAlairas(alairas.getId(), model);
+  }
+
+  @GetMapping("/szerkesztes/{id}")
+  public String szerkesztes(@PathVariable Integer id, Model model) {
+    Alairas alairas = alairasService.findOne(id)
+        .orElseThrow(() -> new ResourceNotFoundException("Aláírás azonosító nem található: " + id));
+
+    model.addAttribute("alairas", alairas);
+//    model.addAttribute("utonevek", utonevService.findAll());
+    return "szerkesztes";
+  }
+
+  @PostMapping("/szerkesztes/{id}")
+  public String szerkesztes(@PathVariable Integer id, @RequestParam String vezeteknev, @RequestParam Integer utonevId,
+      @RequestParam(required = false) Integer utonev_2Id,
+      @RequestParam(required = false) Boolean nemeNo, Model model) {
 //    Alairas alairas = alairasService.findOne(id)
 //        .orElseThrow(() -> new ResourceNotFoundException("Aláírás azonosító nem található: " + id));
 //
-//    model.addAttribute("alairas", alairas);
-//    return "alairas"; // ez a html template jelenjen meg
-//  }
-
-//  @GetMapping("/hozzaadas")
-//  public String hozzaadas(Model model) {
-////    model.addAttribute("utonevek", utonevService.findAll());
-//    return "hozzaadas";
-//  }
-
-//  @PostMapping("/hozzaadas")
-//  public String hozzaadas(@RequestParam String vezeteknev, @RequestParam Integer utonevId,
-//      @RequestParam(required = false) Integer utonev_2Id,
-//      @RequestParam(required = false) Boolean nemeNo, Model model) {
-//    Alairas alairas = alairasService.addOne(vezeteknev, utonevId, utonev_2Id, nemeNo);
+//    alairasService.update(alairas, vezeteknev, utonevId, utonev_2Id, nemeNo);
 //    return getAlairas(alairas.getId(), model);
-//  }
-
-//  @GetMapping("/szerkesztes/{id}")
-//  public String szerkesztes(@PathVariable Integer id, Model model) {
-//    Alairas alairas = alairasService.findOne(id)
-//        .orElseThrow(() -> new ResourceNotFoundException("Aláírás azonosító nem található: " + id));
-//
-//    model.addAttribute("alairas", alairas);
-////    model.addAttribute("utonevek", utonevService.findAll());
-//    return "szerkesztes";
-//  }
-//
-//  @PostMapping("/szerkesztes/{id}")
-//  public String szerkesztes(@PathVariable Integer id, @RequestParam String vezeteknev, @RequestParam Integer utonevId,
-//      @RequestParam(required = false) Integer utonev_2Id,
-//      @RequestParam(required = false) Boolean nemeNo, Model model) {
-////    Alairas alairas = alairasService.findOne(id)
-////        .orElseThrow(() -> new ResourceNotFoundException("Aláírás azonosító nem található: " + id));
-////
-////    alairasService.update(alairas, vezeteknev, utonevId, utonev_2Id, nemeNo);
-////    return getAlairas(alairas.getId(), model);
-//    return "todo";
-//  }
+    return "todo";
+  }
 }
